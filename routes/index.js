885e84880ae92b3bac267cf3855b82f50083fd43
Mikolaj Stanczak <poczta@mikolajstanczak.pl>
2017-06-19 21:12:58 +0200

    code formating
    Added standard for official code formating

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const scrapeService = require('../services/scrapeService');
-const moment = require('moment');
+const express = require('express')
+const router = express.Router()
+const scrapeService = require('../services/scrapeService')
+const moment = require('moment')
 
 router.get('/', (req, res) => {
 
-    scrapeService.getMenus()
-        .then(menus => {
-            res.render('index', {
-                title: 'Lunch w okolicy REGENT OFFICE',
-                today: moment().format('DD.MM.YYYY'),
-                menus
-            });
-        });
-});
+  scrapeService.getMenus()
+    .then(menus => {
+      res.render('index', {
+        title: 'Lunch w okolicy REGENT OFFICE',
+        today: moment().format('DD.MM.YYYY'),
+        menus
+      })
+    })
+})
 
-module.exports = router;
+module.exports = router
