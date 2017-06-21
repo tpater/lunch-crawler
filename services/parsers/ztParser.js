c1fd1f0b22ea9317695b1a9eac95339ef9b92f3b
Mikolaj Stanczak <poczta@mikolajstanczak.pl>
2017-06-21 15:56:18 +0200

    zielone tarasy parser refactor

--- services/parsers/ztParser.js
+++ services/parsers/ztParser.js
@@ -1,20 +1,25 @@
 const helpers = require('../../helpers/helpers')
 const cheerio = require('cheerio')
+const ztService = require('../zieloneTarasyService')
 
-const ztParser = pageObj => {
-  let pageHTML = JSON.parse(pageObj)['data']['document_data']['c2pd']['text']
-  let $ = cheerio.load(pageHTML)
-  let nodesArray = []
-  let nodes = $('p').contents()
+const ztParser = $ => {
+  const page = $.html()
+  return ztService.getZieloneTarasyPageObj(page)
+    .then(pageObj => {
+      let pageHTML = JSON.parse(pageObj)['data']['document_data']['c2pd']['text']
+      let $ = cheerio.load(pageHTML)
+      let nodesArray = []
+      let nodes = $('p').contents()
 
-  //  make an array from objects
-  for (let node in nodes) {
-    if (nodes.hasOwnProperty(node) && typeof nodes[node].data !== 'function' && nodes[node].data) {
-      nodesArray.push(nodes[node].data)
-    }
-  }
+      //  make an array from objects
+      for (let node in nodes) {
+        if (nodes.hasOwnProperty(node) && typeof nodes[node].data !== 'function' && nodes[node].data) {
+          nodesArray.push(nodes[node].data)
+        }
+      }
 
-  return helpers.buildMenu(nodesArray)
+      return helpers.buildMenu(nodesArray)
+    })
 }
 
 module.exports = ztParser
